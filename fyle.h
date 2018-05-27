e0e8deab5cbd81a999c4e7a470cbbee132b20933
Martin Sandiford <ms@mcdev.com.au>
2018-05-27 20:37:47 +0930

    Fixed argument for file_name_str in file_create_open.

--- fyle.h
+++ fyle.h
@@ -7,7 +7,7 @@ void file_name(file_ptr fp, int max_len, file_name_str &act_fnm, int &act_len);
 void file_table();
 void file_fix_eop(bool eof, line_ptr eop_line);
 
-bool file_create_open(const file_name_str &fn, parse_type parse, file_ptr &inputfp, file_ptr &outputfp);
+bool file_create_open(file_name_str &fn, parse_type parse, file_ptr &inputfp, file_ptr &outputfp);
 bool file_close_delete(file_ptr &fp, bool delet, bool messages);
 bool file_read(file_ptr fp, line_range count, bool best_try, line_ptr &first, line_ptr &last, int &actual_cnt);
 bool file_write(line_ptr first_line, line_ptr last_line, file_ptr fp);
