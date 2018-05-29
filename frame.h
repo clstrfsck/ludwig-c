b1e4a96b69d9c423d194a1fb88ba1492962fa245
Martin Sandiford <ms@mcdev.com.au>
2018-05-29 21:47:49 +0930

    Initial version frame.h.

---
+++ frame.h
@@ -0,0 +1,12 @@
+#ifndef FRAME_H
+#define FRAME_H
+
+#include "type.h"
+
+bool frame_edit(const name_str &frame_name);
+bool frame_kill(const name_str &frame_name);
+
+bool frame_setheight(int sh, bool set_initial);
+bool frame_parameter(tpar_ptr tpar);
+
+#endif
