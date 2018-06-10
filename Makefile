2ce1a785ea9260de905104da58164e05eed376dc
Martin Sandiford <ms@mcdev.com.au>
2018-06-10 22:08:15 +0930

    Added dependencies and help file to Makefile. Added help source to git.

--- Makefile
+++ Makefile
@@ -39,14 +39,119 @@ OBJS = \
 	window.o \
 	word.o
 
-.cpp.o:
-	clang++ -fdiagnostics-color=never -g -c -Wall -std=c++14 -DDEBUG -DWINDOWCHANGE -D_GLIBCXX_DEBUG $*.cpp
+# Either g++ or clang++ should work.
+# Tested with clang++ 6.0.0 and g++ 7.3.0
+CXX = g++
+CC = gcc
+# These are debug flags. Works for both g++ and clang++.
+CXXFLAGS = -fdiagnostics-color=never -g -Wall -std=c++14 -DDEBUG -DWINDOWCHANGE -D_GLIBCXX_DEBUG
+CFLAGS = -fdiagnostics-color=never -g -Wall -DMKSTEMP
+# These are release flags. Works for g++, clang++ doesn't understand -Wno-maybe-uninitialized
+#CXXFLAGS = -fdiagnostics-color=never -Wno-maybe-uninitialized -O3 -Wall -std=c++14 -DWINDOWCHANGE
+#CFLAGS = -fdiagnostics-color=never -O3 -Wall -DMKSTEMP
 
-all:	ludwig
+all:	ludwig ludwighlp.idx ludwignewhlp.idx
 	echo Done.
 
 ludwig:	$(OBJS)
-	clang++ -lncurses -o ludwig $(OBJS)
+	$(CXX) -o ludwig $(OBJS) -lncurses
+
+ludwighlpbld:	ludwighlpbld.c
+	$(CC) $(CFLAGS) -o ludwighlpbld ludwighlpbld.c
+
+ludwighlp.idx:  ludwighlpbld ludwighlp.t
+	./ludwighlpbld ludwighlp.t ludwighlp.idx
+
+ludwignewhlp.idx:  ludwighlpbld ludwignewhlp.t
+	./ludwighlpbld ludwignewhlp.t ludwignewhlp.idx
 
 clean:
-	rm -f *.o
+	rm -f $(OBJS) ludwighlpbld ludwighlp.idx ludwignewhlp.idx
+
+arrow.o: arrow.cpp arrow.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h vdu.h line.h mark.h text.h screen.h
+caseditto.o: caseditto.cpp caseditto.h type.h const.h parray.h prange.h \
+ perange.h penumset.h prangeset.h var.h vdu.h mark.h text.h screen.h
+charcmd.o: charcmd.cpp charcmd.h type.h const.h parray.h prange.h \
+ perange.h penumset.h prangeset.h var.h vdu.h mark.h text.h screen.h
+ch.o: ch.cpp ch.h type.h const.h parray.h prange.h perange.h penumset.h \
+ prangeset.h
+code.o: code.cpp code.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h vdu.h exec.h line.h mark.h tpar.h frame.h \
+ screen.h
+const.o: const.cpp const.h
+dfa.o: dfa.cpp dfa.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h screen.h
+eqsgetrep.o: eqsgetrep.cpp eqsgetrep.h type.h const.h parray.h prange.h \
+ perange.h penumset.h prangeset.h ch.h var.h dfa.h mark.h text.h screen.h \
+ charcmd.h patparse.h recognize.h
+exec.o: exec.cpp exec.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h vdu.h code.h fyle.h help.h line.h mark.h \
+ quit.h span.h swap.h text.h tpar.h user.h word.h arrow.h frame.h opsys.h \
+ screen.h window.h charcmd.h newword.h validate.h caseditto.h eqsgetrep.h \
+ nextbridge.h
+execimmed.o: execimmed.cpp execimmed.h type.h const.h parray.h prange.h \
+ perange.h penumset.h prangeset.h var.h vdu.h code.h exec.h fyle.h line.h \
+ mark.h quit.h text.h screen.h
+filesys.o: filesys.cpp filesys.h type.h const.h parray.h prange.h \
+ perange.h penumset.h prangeset.h screen.h lwgetopt.h
+frame.o: frame.cpp frame.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h ch.h dfa.h var.h vdu.h line.h mark.h span.h \
+ tpar.h user.h screen.h version.h
+fyle.o: fyle.cpp fyle.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h vdu.h exec.h line.h mark.h tpar.h screen.h \
+ filesys.h
+help.o: help.cpp help.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h ch.h var.h screen.h helpfile.h
+helpfile.o: helpfile.cpp type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h
+line.o: line.cpp line.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h screen.h
+ludwig.o: ludwig.cpp var.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h vdu.h exec.h fyle.h quit.h user.h frame.h msdos.h \
+ value.h screen.h filesys.h execimmed.h
+lwgetopt.o: lwgetopt.cpp lwgetopt.h
+mark.o: mark.cpp mark.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h screen.h
+msdos.o: msdos.cpp msdos.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h
+newword.o: newword.cpp newword.h type.h const.h parray.h prange.h \
+ perange.h penumset.h prangeset.h var.h line.h mark.h text.h
+nextbridge.o: nextbridge.cpp nextbridge.h type.h const.h parray.h \
+ prange.h perange.h penumset.h prangeset.h var.h mark.h
+opsys.o: opsys.cpp opsys.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h line.h filesys.h
+parray.o: parray.cpp type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h
+patparse.o: patparse.cpp patparse.h type.h const.h parray.h prange.h \
+ perange.h penumset.h prangeset.h var.h tpar.h screen.h
+quit.o: quit.cpp quit.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h vdu.h fyle.h mark.h msdos.h screen.h
+recognize.o: recognize.cpp recognize.h type.h const.h parray.h prange.h \
+ perange.h penumset.h prangeset.h var.h
+screen.o: screen.cpp screen.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h vdu.h line.h
+span.o: span.cpp span.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h code.h fyle.h line.h mark.h screen.h
+swap.o: swap.cpp swap.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h mark.h text.h
+text.o: text.cpp text.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h vdu.h line.h mark.h screen.h
+tpar.o: tpar.cpp tpar.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h ch.h var.h span.h msdos.h screen.h
+user.o: user.cpp user.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h vdu.h code.h mark.h span.h text.h tpar.h \
+ msdos.h screen.h
+validate.o: validate.cpp validate.h type.h const.h parray.h prange.h \
+ perange.h penumset.h prangeset.h var.h screen.h
+value.o: value.cpp value.h var.h type.h const.h parray.h prange.h \
+ perange.h penumset.h prangeset.h
+var.o: var.cpp var.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h
+vdu.o: vdu.cpp vdu.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h
+version.o: version.cpp version.h
+window.o: window.cpp window.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h vdu.h line.h mark.h frame.h screen.h
+word.o: word.cpp word.h type.h const.h parray.h prange.h perange.h \
+ penumset.h prangeset.h var.h line.h mark.h text.h screen.h
---
+++ ludwighlpbld.c
@@ -0,0 +1,240 @@
+/**********************************************************************/
+/*                                                                    */
+/*           L      U   U   DDDD   W      W  IIIII   GGGG             */
+/*           L      U   U   D   D   W    W     I    G                 */
+/*           L      U   U   D   D   W ww W     I    G   GG            */
+/*           L      U   U   D   D    W  W      I    G    G            */
+/*           LLLLL   UUU    DDDD     W  W    IIIII   GGGG             */
+/*                                                                    */
+/**********************************************************************/
+/*                                                                    */
+/*  Copyright (C) 1981, 1987                                          */
+/*  Department of Computer Science, University of Adelaide, Australia */
+/*  All rights reserved.                                              */
+/*  Reproduction of the work or any substantial part thereof in any   */
+/*  material form whatsoever is prohibited.                           */
+/*                                                                    */
+/**********************************************************************/
+
+/*
+ * Name:        LUDWIGHLP
+ *
+ * Description: This program converts a sequential Ludwig help file into
+ *              an indexed file for fast access.
+ *
+ * Revision History:
+ * 4-001 Ludwig V4.0 release.                                 7-Apr-1987
+ * 4-002 Kelvin B. Nicolle                                    5-May-1987
+ *       The input text has been reformatted so that column one contains
+ *       only flag characters.
+ * 4-003 Jeff Blows                                          23-Jun-1989
+ *       Merge changes needed to compile on MS-DOS
+ * 4-004 Ludwig                                              28-Feb-1990
+ *       Stop complaining about line too long when it's a comment.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#if defined(MACOS) || defined(linux)
+#include <unistd.h>
+#endif
+
+#define BUFSIZE         1024
+#define ENTRYSIZE       78      /* 77 + 1 for NUL */
+#define KEYSIZE         4
+
+void process_files(FILE *, FILE *);
+
+int
+main(argc,argv)
+long argc;
+char *argv[];
+{
+    char infile[BUFSIZE],outfile[BUFSIZE];
+    FILE *in,*out;
+
+    if (--argc > 0)
+	strcpy(infile,*++argv);
+    else
+#ifdef turboc
+	strcpy(infile,"ludwighl.txt");
+#else
+	strcpy(infile,"ludwighlp.t");
+#endif
+    if ((in = fopen(infile,"r")) == NULL) {
+	perror(infile);
+	exit(1);
+    }
+    if (--argc > 0)
+	strcpy(outfile,*++argv);
+    else
+#ifdef turboc
+	strcpy(outfile,"ludwighl.idx");
+#else
+	strcpy(outfile,"ludwighlp.idx");
+#endif
+    if ((out = fopen(outfile,"w+")) == NULL) {
+	perror(outfile);
+	exit(1);
+    }
+    process_files(in,out);
+    printf("Conversion complete.\n");
+    exit(0);
+}
+
+void
+process_files(in,out)
+FILE *in,*out;
+
+{
+# ifdef MKSTEMP
+    int f1, f2, f3;
+# else
+    char *f1, *f2, *f3;
+# endif
+    char flag,line[ENTRYSIZE+1],section[KEYSIZE+1];
+    long i,len,index_lines,contents_lines;
+    FILE *index,*body,*contents;
+# ifndef ultrix
+    char *mktemp();
+    char temp1[] = "LWXXXXXX";
+    char temp2[] = "LWXXXXXX";
+    char temp3[] = "LWXXXXXX";
+# endif
+
+    index_lines = contents_lines = 0;
+    strcpy(section,"0");
+    /*
+     * Create two temporary files to store the index and the actual info
+     * while we work through the file.
+     */
+# ifdef ultrix
+    if ((index = fopen(f1 = tempnam("/tmp","hlp"),"w+")) == NULL) {
+	perror("tmpfile");
+	exit(1);
+    }
+    if ((body = fopen(f2 = tempnam("/tmp","hlp"),"w+")) == NULL) {
+	perror("tmpfile");
+	exit(1);
+    }
+    if ((contents = fopen(f3 = tempnam("/tmp","hlp"),"w+")) == NULL) {
+	perror("tmpfile");
+	exit(1);
+    }
+# else
+#  ifdef MKSTEMP
+    if ((index = fdopen(f1 = mkstemp(temp1), "w+")) == NULL) {
+        perror("tmpfile");
+        exit(1);
+    }
+    if ((body = fdopen(f2 = mkstemp(temp2), "w+")) == NULL) {
+	perror("tmpfile");
+	exit(1);
+    }
+    if ((contents = fdopen(f3 = mkstemp(temp3), "w+")) == NULL) {
+	perror("tmpfile");
+	exit(1);
+    }
+#  else
+    if ((index = fopen(f1 = mktemp(temp1),"w+")) == NULL) {
+	perror("tmpfile");
+	exit(1);
+    }
+    if ((body = fopen(f2 = mktemp(temp2),"w+")) == NULL) {
+	perror("tmpfile");
+	exit(1);
+    }
+    if ((contents = fopen(f3 = mktemp(temp3),"w+")) == NULL) {
+	perror("tmpfile");
+	exit(1);
+    }
+#  endif
+# endif
+    do {
+	flag = fgetc(in);
+	if (flag == EOF)
+	    break;
+	else if (flag == '\n') {
+	    flag = ' ';
+	    len = 0;
+	    line[0] = '\0';
+	} else {
+	    if (fgets(line,ENTRYSIZE+1,in) == NULL) break;
+	    len = strlen(line);
+	    if (len == ENTRYSIZE && line[ENTRYSIZE-1] != '\n') {
+		if (flag != '!' && flag != '{') {
+		  fprintf(stderr,"Line too long--truncated\n");
+		  fprintf(stderr,"%*s>>\n",ENTRYSIZE,line);
+		}
+		while (fgetc(in) != '\n') continue;
+	    }
+	    line[len-1] = '\0';
+	}
+	switch (flag) {
+	    case '\\' :
+		switch (line[0]) {
+		    case '%' :
+			fputs("\\%\n",body);
+			break;
+		    case '#' :
+			if (strcmp(section,"0") != 0)
+			    fprintf(index,"%8ld\n",ftell(body));
+			break;
+		    default :
+			if (strcmp(section,"0") != 0)
+			    fprintf(index,"%8ld\n",ftell(body));
+			for (i = 0;i < KEYSIZE && line[i];i++)
+			    section[i] = line[i];
+			section[i] = '\0';
+			if (strcmp(section,"0") != 0) {
+			    index_lines++;
+			    fprintf(index,"%4s %8ld",section,ftell(body));
+			}
+			break;
+		}
+		break;
+	    case '+' :
+		contents_lines++;
+		fprintf(contents,"%s\n",line);
+		fprintf(body,"%s\n",line);
+		break;
+	    case ' ' :
+		if (strcmp(section,"0") == 0) {
+		    contents_lines++;
+		    fprintf(contents,"%s\n",line);
+		} else
+		    fprintf(body,"%s\n",line);
+		break;
+	    case '{' :
+	    case '!' :
+		break;
+	    default :
+		fprintf(stderr,"Illegal flag character.\n");
+		fprintf(stderr,"%c%s>>\n",flag,line);
+		break;
+	}
+    } while (!(flag == '\\' && line[0] == '#'));
+    rewind(index);
+    rewind(body);
+    rewind(contents);
+    fprintf(out,"%ld %ld\n",index_lines,contents_lines);
+    while (fgets(line,ENTRYSIZE+1,index) != NULL)
+	fputs(line,out);
+    while (fgets(line,ENTRYSIZE+1,contents) != NULL)
+	fputs(line,out);
+    while (fgets(line,ENTRYSIZE+1,body) != NULL)
+	fputs(line,out);
+    fclose(index);
+    fclose(contents);
+    fclose(body);
+#ifdef MKSTEMP
+    unlink(temp1);
+    unlink(temp2);
+    unlink(temp3);
+#else
+    unlink(f1);
+    unlink(f2);
+    unlink(f3);
+#endif
+}
