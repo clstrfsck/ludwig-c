9ea8d7a8fcaab07ed9ca1f04645037ddbb46cb4c
Martin Sandiford <ms@mcdev.com.au>
2018-06-10 17:01:07 +0930

    Added additional bounds checking to parray.h.

--- parray.h
+++ parray.h
@@ -22,6 +22,12 @@ class parray {
         return R::zero_based(index);
     }
 
+    inline void check_index(typename R::type index) const {
+        if (index < R::min() || index > R::max()) {
+            throw std::out_of_range("index out of range");
+        }
+    }
+
     typedef std::vector<T> array_type;
 
 public:
@@ -56,22 +62,30 @@ public:
     }
 
     reference operator[] (typename index_type::type index) {
+        check_index(index);
         return m_array[adjust_index(index)];
     }
 
     const_reference operator[] (typename index_type::type index) const {
+        check_index(index);
         return m_array[adjust_index(index)];
     }
 
     parray &copy(const_pointer src, size_t count, typename index_type::type dst_offset = index_type::min()) {
-        size_t offset_index = adjust_index(dst_offset);
-        typename array_type::iterator i = std::next(m_array.begin(), offset_index);
-        std::copy(src, src + count, i);
+        if (count > 0) {
+            check_index(dst_offset); // First index used
+            check_index(dst_offset + count - 1); // Last index used
+            size_t offset_index = adjust_index(dst_offset);
+            typename array_type::iterator i = std::next(m_array.begin(), offset_index);
+            std::copy(src, src + count, i);
+        }
         return *this;
     }
 
     parray &fill(const T &value, typename index_type::type begin = index_type::min(),
                  typename index_type::type endi = index_type::max()) {
+        check_index(begin);
+        check_index(endi);
         size_t ibeg = adjust_index(begin);
         size_t iend = adjust_index(endi) + 1;
         if (ibeg < iend) {
@@ -83,15 +97,21 @@ public:
     }
 
     parray &fill_n(const T &value, size_t n, typename index_type::type begin = index_type::min()) {
-        size_t ibeg = adjust_index(begin);
-        typename array_type::iterator b = std::next(m_array.begin(), ibeg);
-        typename array_type::iterator e = std::next(b, n);
-        std::fill(b, e, value);
+        if (n > 0) {
+            check_index(begin);
+            check_index(begin + n - 1);
+            size_t ibeg = adjust_index(begin);
+            typename array_type::iterator b = std::next(m_array.begin(), ibeg);
+            typename array_type::iterator e = std::next(b, n);
+            std::fill(b, e, value);
+        }
         return *this;
     }
 
     parray &apply(std::function<T(T)> f, typename index_type::type beg = index_type::min(),
                   typename index_type::type endi = index_type::max()) {
+        check_index(beg);
+        check_index(endi);
         size_t ibeg = adjust_index(beg);
         size_t iend = adjust_index(endi) + 1;
         if (ibeg < iend) {
@@ -104,6 +124,8 @@ public:
 
     parray &apply_n(std::function<T(T)> f, size_t n, typename index_type::type beg = index_type::min()) {
         if (n > 0) {
+            check_index(beg);
+            check_index(beg + n - 1);
             size_t ibeg = adjust_index(beg);
             typename array_type::iterator b = std::next(m_array.begin(), ibeg);
             typename array_type::iterator e = std::next(b, n);
@@ -112,30 +134,38 @@ public:
         return *this;
     }
 
-    parray &fillcopy(const_pointer src, size_t src_len, typename index_type::type dst_index, size_t dst_len, const_reference value) {
-        size_t len = std::min(src_len, dst_len);
-        size_t dst = adjust_index(dst_index);
-        if (len != 0) {
-            std::copy(src, src + len, std::next(m_array.begin(), dst));
-        }
-        if (dst_len > len) {
-            typename array_type::iterator db = std::next(m_array.begin(), dst + len);
-            typename array_type::iterator de = std::next(db, dst_len - len);
-            std::fill(db, de, value);
+    parray &fillcopy(const_pointer src, size_t src_len,
+                     typename index_type::type dst_index, size_t dst_len, const_reference value) {
+        if (dst_len > 0) {
+            check_index(dst_index);
+            check_index(dst_index + dst_len - 1);
+            size_t len = std::min(src_len, dst_len);
+            size_t dst = adjust_index(dst_index);
+            if (len != 0) {
+                std::copy(src, src + len, std::next(m_array.begin(), dst));
+            }
+            if (dst_len > len) {
+                typename array_type::iterator db = std::next(m_array.begin(), dst + len);
+                typename array_type::iterator de = std::next(db, dst_len - len);
+                std::fill(db, de, value);
+            }
         }
         return *this;
     }
 
     const_pointer data(typename index_type::type index = index_type::min()) const {
+        check_index(index);
         return m_array.data() + adjust_index(index);
     }
 
     pointer data(typename index_type::type index = index_type::min()) {
+        check_index(index);
         return m_array.data() + adjust_index(index);
     }
 
     size_t length(const_reference value, typename index_type::type from = index_type::max()) const {
         if (!m_array.empty()) {
+            check_index(from);
             int last = adjust_index(from);
             while (last >= 0) {
                 if (m_array[last] != value)
@@ -153,16 +183,7 @@ public:
     }
 
     bool operator==(const parray &rhs) const {
-        const_pointer l = data();
-        const_pointer r = rhs.data();
-        const_pointer endl = l + index_type::size();
-        while (l != endl) {
-            const T &lelt = *l++;
-            const T &relt = *r++;
-            if (lelt != relt)
-                return false;
-        }
-        return true;
+        return m_array == rhs.m_array;
     }
 
     bool operator!=(const parray &rhs) const {
@@ -170,18 +191,7 @@ public:
     }
 
     bool operator<(const parray &rhs) const {
-        const_pointer l = data();
-        const_pointer r = rhs.data();
-        const_pointer endl = l + index_type::size();
-        while (l != endl) {
-            const T &lelt = *l++;
-            const T &relt = *r++;
-            if (lelt < relt)
-                return true;
-            if (lelt != relt)
-                break;
-        }
-        return false;
+        return std::lexicographical_compare(m_array.begin(), m_array.end(), rhs.m_array.begin(), rhs.m_array.end());
     }
 
 private:
